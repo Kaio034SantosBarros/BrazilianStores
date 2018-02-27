44e59363e9c4ed041eb92576f244792172db098d
Cristiano-Medeiros-Camarinho <crislive.com.pt@gmail.com>
2018-02-27 11:58:52 -0300

    Cris2

--- projeto6/perfil.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	if(empty($_SESSION['email'])){
-		header("Location: index.php");
-	}
-
-	echo "Você está conectado!";
-	echo "<button type='submit'><a href='sair.php'>Sair</a></button>";
-?>
--- projeto6/sair.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	session_start();
-	if(empty($_SESSION['email'])){
-		header("Location: index.php");
-	}
-	session_destroy();
-	header("Refresh: 3, index.php");
-	echo "Saindo...";
- ?>
